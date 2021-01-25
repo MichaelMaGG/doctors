c6c383fe56208c577d6434f2908db630c06e54e9
Michael <michael2110@sina.com>
2021-01-25 19:41:43 +1300

    init project

---
+++ src/main/java/nz/co/doctorS/DoctorS/DoctorSApplication.java
@@ -0,0 +1,13 @@
+package nz.co.doctorS.DoctorS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorSApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nz/co/doctorS/DoctorS/DoctorSApplicationTests.java
@@ -0,0 +1,13 @@
+package nz.co.doctorS.DoctorS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorSApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
